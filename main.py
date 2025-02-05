dc72cbc8a7306c5d1839f0a94d929d66e59d9aba
lucapdr1 <luca.pedranzini@mail.polimi.it>
2025-02-05 10:47:59 +0100

    feat: blocks and intra blocks

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from core.optimization_experiment import OptimizationExperiment
 from core.ordering.rule_combination import RuleComposition
+from core.ordering.rule_combination_hierarchical import HierarchicalRuleComposition
 from core.ordering.variable_type_rule import VariableTypeRule
 from core.ordering.objective_coefficient_rule import ObjectiveCoefficientRule
 from core.ordering.cols_coefficient_rule import ColumnsCoefficientRule
@@ -12,21 +13,36 @@ from core.ordering.row_coefficient_rule import RowCoefficientRule
 from core.ordering.constraint_range_rule import ConstraintRangeRule
 from utils.config import INPUT_PROBLEM, NUMBER_OF_PERMUATATIONS
 
-rules = [
-    #cols
-    VariableTypeRule(1e5),
+var_block_rules = [
+    VariableTypeRule(1),  # e.g., BINARY->3, INTEGER->2, CONTINUOUS->1
+    # You could add more block rules here
+]
+
+var_intra_rules = [
     ColumnsCoefficientRule(1),
     ObjectiveCoefficientRule(100),
-    VariableOccurrenceRule(1),
-    #rows
-    ConstraintSenseRule(1e5),
+    VariableOccurrenceRule(1)
+]
+
+constr_block_rules = [
+    ConstraintSenseRule(1),
+    # Possibly more block rules
+]
+
+constr_intra_rules = [
     RowCoefficientRule(1),
     RHSValueRule(100),
     ConstraintRangeRule(1)
-    
-
 ]
-ordering_rule = RuleComposition(rules)
+
+ordering_rule = HierarchicalRuleComposition(
+    var_block_rules=var_block_rules,
+    var_intra_rules=var_intra_rules,
+    constr_block_rules=constr_block_rules,
+    constr_intra_rules=constr_intra_rules,
+    block_rank_factor=1e6
+)
+
 
 if __name__ == "__main__":    
     try:
