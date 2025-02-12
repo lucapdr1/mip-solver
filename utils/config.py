86919ebaf2c4e23a676eed1ad7b1e3134605ecf6
lucapdr1 <luca.pedranzini@mail.polimi.it>
2025-02-12 17:48:53 +0100

    feat: normalization with bugs

--- utils/config.py
+++ utils/config.py
@@ -18,7 +18,7 @@ INPUT_PROBLEM =  os.getenv('INPUT_PROBLEM', "dummy.mps")
 #INPUT_PROBLEM = os.getenv('INPUT_PROBLEM', "net12.mps")
 
 NUMBER_OF_PERMUATATIONS = int(os.getenv('NUMBER_OF_PERMUATATIONS', 3))
-NORMALIZATION_ACTIVE = os.getenv('NORMALIZATION_ACTIVE', 'False').lower() == 'true'
+NORMALIZATION_ACTIVE = os.getenv('NORMALIZATION_ACTIVE', 'True').lower() == 'true'
 SCALING_ACTIVE = os.getenv('SCALING_ACTIVE', 'True').lower() == 'true' 
 
 MATRICES_TO_CSV = os.getenv('MATRICES_TO_CSV', 'False').lower() == 'true'
